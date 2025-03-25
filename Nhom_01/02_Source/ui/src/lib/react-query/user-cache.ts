fba01b889475b2204dedcff6fe4a593067aa6d43
tmplam <phulam.dev@gmail.com>
2025-03-25 22:30:39 +0700

    remove reduntdant import in user cache

--- Nhom_01/02_Source/ui/src/lib/react-query/user-cache.ts
+++ Nhom_01/02_Source/ui/src/lib/react-query/user-cache.ts
@@ -1,13 +1,13 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
 import { commonOptions } from "@/lib/react-query/options";
-import { callGettingUserInfoRequest } from "@/api/user.api";
+// import { callGettingUserInfoRequest } from "@/api/user.api";
 
 export function useCachedUserInfo() {
   return useQuery({
     queryKey: ["user-info"],
     queryFn: () => {
-      return callGettingUserInfoRequest();
+      // return callGettingUserInfoRequest();
     },
     throwOnError: true,
     ...commonOptions
