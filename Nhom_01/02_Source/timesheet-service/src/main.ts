8888b9cea04069eba1548f272e76885501d0c4a3
khiemta03 <khiemta.dev@gmail.com>
2025-04-03 16:28:56 +0000

    feat: add swaggers

--- Nhom_01/02_Source/timesheet-service/src/main.ts
+++ Nhom_01/02_Source/timesheet-service/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from '@/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { VersioningType } from '@nestjs/common';
 import { ENV } from '@/libs/configs/env';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,15 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: ENV.app_version,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('KIMAI')
+    .setDescription('API Documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(ENV.app_port ?? 3000);
 }
